cf9af5e212972e7b6bb9abbf9b0992b34f7bb9eb
Pavlov Vladimir <connection.lost.again@gmail.com>
2022-11-22 00:41:26 +0600

    Created post form

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,7 @@
-import React, {SetStateAction, useEffect, useRef, useState} from 'react';
+import React, {FormEvent, useEffect, useRef, useState} from 'react';
 import './App.css';
+import {Button, FormControl, Input, InputLabel, TextareaAutosize} from "@mui/material";
+
 
 type Message = {
   message: string,
@@ -18,8 +20,38 @@ const request = async (url: string): Promise<Message[]> => {
 
 function App() {
   const [messages, setMessages] = useState<Message[]>([]);
+  const [isNewFirst, setIsNewFirst] = useState(true);
+  const [nickname, setNickname] = useState('');
+  const [message, setMessage] = useState('');
   const intervalId = useRef<null | number>(null);
   const param = useRef<string>('');
+
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  };
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  }
+
+  const changeMessageLayout = () => {
+    setIsNewFirst(prev => !prev);
+    console.log('New first: ', isNewFirst);
+  };
+
+  const sendMessage = async (e: FormEvent) => {
+    console.log('submitted')
+    e.preventDefault();
+    const data = new URLSearchParams();
+    data.set('message', message);
+    data.set('author', nickname);
+    await fetch(messagesUrl, {
+      method: 'post',
+      body: data,
+    });
+
+    setMessage('');
+  };
 
   const getMessages = async (url: string) => {
     const messages: Message[] = await request(url);
@@ -39,6 +71,20 @@ function App() {
     autoRefresh();
   }, []);
 
+  const renderMessage = (message: Message) => {
+    const date = new Date(message.datetime);
+    const dateString = date.toLocaleTimeString() + ' ' + date.toLocaleDateString();
+    return (
+      <div key={message._id} className="message">
+        <div className="message-header">
+          <span>From</span> {message.author}
+        </div>
+        <div className="message-body">{message.message}</div>
+        <div className="message-footer"><span>Date: </span>{dateString}</div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <div className="App-header">
@@ -46,29 +92,24 @@ function App() {
           <h1>Chat</h1>
           <div className="options">
             <label htmlFor="setting-show-new">New messages first</label>
-            <input type="checkbox" id="setting-show-new"/>
+            <input type="checkbox" id="setting-show-new" checked={isNewFirst} onChange={changeMessageLayout}/>
           </div>
         </div>
       </div>
       <div className="App-main">
         <div className="container">
           <div className="messages">
-            {messages.map((message) => {
-              const date = new Date(message.datetime);
-              const dateString = date.toLocaleTimeString() + ' ' + date.toLocaleDateString();
-              return (
-                <div key={message._id} className="message">
-                  <div className="message-header">
-                   <span>From</span> {message.author}
-                  </div>
-                  <div className="message-body">{message.message}</div>
-                  <div className="message-footer"><span>Date: </span>{dateString}</div>
-                </div>
-              );
-            })}
+            {!isNewFirst ? messages.map(renderMessage) : [...messages].reverse().map(renderMessage)}
           </div>
           <div className="sidebar">
-            sidebar
+            <form onSubmit={sendMessage}>
+              <FormControl>
+                <InputLabel htmlFor="nickname">Nickname</InputLabel>
+                <Input id="nickname" value={nickname} onChange={handleNicknameChange}/>
+                <TextareaAutosize placeholder="Message here..." value={message} onChange={handleMessageChange}/>
+                <Button type="submit">Send</Button>
+              </FormControl>
+            </form>
           </div>
         </div>
       </div>
